ffb6750c5e2127da449324dbdf88d1b70ffe57c2
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-09-06 12:11:58 +0200

    remove old test

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -4,6 +4,7 @@ import java.time.LocalDate
 
 fun main() {
     println("Please enter a date with the format <yyy-MM-dd>")
-    val input = LocalDate.parse(readlnOrNull())
+    val readlLine: String? = null
+    val input = LocalDate.parse(readlLine ?: "2023-04-09")
     println("You wrote $input ${input.dayOfWeek}")
 }
