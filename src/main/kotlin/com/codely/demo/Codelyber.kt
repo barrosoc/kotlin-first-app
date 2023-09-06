ccd9c4b9b5bc4594792c7e771b7ebefd39836df2
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-09-06 12:51:26 +0200

    add scope functions to the code

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,12 +1,21 @@
 package com.codely.demo
 
 import java.time.LocalDate
+import java.time.Period
 
 fun main() {
-    println("Please enter a date with the format <yyy-MM-dd>")
-    val readLine: String? = null
-    readLine?.let {
-        val input = LocalDate.parse(readLine)
-        println("You wrote $input ${input.dayOfWeek}")
+    println("Please enter your birthday with the format <yyy-MM-dd>")
+    supportNullableString(readlnOrNull()).takeUnless {
+        it.isNullOrBlank() || it.isEmpty()
+    }?.let { line ->
+        val input = LocalDate.parse(line).also {
+            println("Your birthday is $it")
+        }
+
+        with(Period.between(input, LocalDate.now())) {
+            println("The difference between your birthday and the current date is ${this.years} years")
+        }
     }
 }
+
+fun supportNullableString(line: String?) = line
