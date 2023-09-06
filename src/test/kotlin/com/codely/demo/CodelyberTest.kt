c736516d728f0bcb5fda5b3175e726854959d0e7
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-09-06 12:13:22 +0200

    fix format class

--- src/test/kotlin/com/codely/demo/CodelyberTest.kt
+++ src/test/kotlin/com/codely/demo/CodelyberTest.kt
@@ -1,12 +1,3 @@
 package com.codely.demo
 
-import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
-
-class CodelyberTest {
-    @Test
-    fun `it should greet successfully`() {
-        val expectedGreet = "Welcome to kotlin skeleton!!!"
-        assertEquals(expectedGreet, Codelyber().greet())
-    }
-}
+class CodelyberTest
