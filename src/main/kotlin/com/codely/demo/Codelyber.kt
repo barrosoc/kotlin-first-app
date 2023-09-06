63412f7b09281c15483baed625b0c9ba69beb79f
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-09-06 12:17:39 +0200

    manage nulls

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -4,7 +4,9 @@ import java.time.LocalDate
 
 fun main() {
     println("Please enter a date with the format <yyy-MM-dd>")
-    val readlLine: String? = null
-    val input = LocalDate.parse(readlLine ?: "2023-04-09")
-    println("You wrote $input ${input.dayOfWeek}")
+    val readLine: String? = null
+    readLine?.let {
+        val input = LocalDate.parse(readLine)
+        println("You wrote $input ${input.dayOfWeek}")
+    }
 }
