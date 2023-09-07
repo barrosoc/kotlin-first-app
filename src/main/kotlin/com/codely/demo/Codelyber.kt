8b6eb74e7e79e902bf0da7c939db2be91f0ac4a3
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-09-07 10:26:46 +0200

    add when statement for showing the proper message

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -2,18 +2,28 @@ package com.codely.demo
 
 import java.time.LocalDate
 import java.time.Period
+import kotlin.system.exitProcess
 
 fun main() {
     println("Please enter your birthday with the format <yyy-MM-dd>")
-    supportNullableString(readlnOrNull()).takeUnless {
-        it.isNullOrBlank() || it.isEmpty()
+    supportNullableString(readlnOrNull()).takeIf {
+        !it.isNullOrBlank() && it.isNotEmpty()
     }?.let { line ->
-        val input = LocalDate.parse(line).also {
-            println("Your birthday is $it")
+        LocalDate.parse(line)
+    }.apply {
+        if(this == null) {
+            println("The input date is not valid")
+            exitProcess(1)
         }
-
-        with(Period.between(input, LocalDate.now())) {
-            println("The difference between your birthday and the current date is ${this.years} years")
+    }.also {
+        println("Your birthday is $it")
+    }.run {
+        with(Period.between(this, LocalDate.now())) {
+            when {
+                years > 0 -> println("The difference between your birthday and the current date is $years years")
+                months > 0 -> println("The difference between your birthday and the current date is $months months")
+                days > 0 -> println("The difference between your birthday and the current date is $days days")
+            }
         }
     }
 }
